d107e5259c5c09ee34e4fefb28c5c9f35558368e
Tairesis <marcomarzos9@gmail.com>
2023-10-29 09:04:03 -0300

    modificaciones carrusel

--- carrusel.js
+++ carrusel.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         contenedorImagenes.style.width = imagenes.length * 100 + 'px';
-        contenedorImagenes.style.transform = translateX(-indiceActual * 100);
+        contenedorImagenes.style.transform = 'translateX(' + (-indiceActual * 100) + 'px)';
     }
 
     function siguienteImagen() {
